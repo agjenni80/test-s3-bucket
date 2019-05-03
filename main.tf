3fb12a40ae9d7d18df4e3b85aa74e74b0292419d
Amanda Jennings <38109573+agjenni80@users.noreply.github.com>
2019-05-03 09:40:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 0.11.7"
+  required_version = ">= 0.11.13"
 }
 
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-1"
+  default = "us-gov-west-1"
 }
 
 variable "bucket_name" {
-   description = "Name of the bucket to create"
+   description = "ajennings-govcloud"
 }
 
 variable "bucket_acl" {
@@ -27,7 +27,7 @@ resource "aws_s3_bucket" "bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "arn:aws:kms:us-east-1:753646501470:key/00c892e8-40c4-4048-a650-0f755876503d"
+        kms_master_key_id = "arn:aws-us-gov:kms:us-gov-west-1:919784536822:key/24ddabfc-c4a2-4323-bed5-220b4bb4b80d"
         sse_algorithm     = "aws:kms"
       }
     }
@@ -39,18 +39,18 @@ resource "aws_s3_bucket" "bucket" {
   }
   
   logging {
-    target_bucket = "roger-bucket"
+    target_bucket = "amanda-bucket"
   }
   
   tags {
-    name        = "Roger Test Bucket"
-    Owner = "roger@hashicorp.com"
+    name        = "Amanda Test Bucket"
+    Owner = "ajennings@hashicorp.com"
     website = "true"
     application_id = "456"
-    stack_name = "roger"
-    description = "roger"
-    termination_date = "12/25/2019"
-    created_by = "roger"
+    stack_name = "ajennings"
+    description = "ajennings"
+    termination_date = "05/26/2019"
+    created_by = "amanda"
     data_class = "protected"
     environment = "dev" 
   }
